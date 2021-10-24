f1ee68f6ab1c0ef6b11860c2612b251d9501b17d
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2021-10-24 19:19:23 +0300

    fixed role dao to look for enum, and not string

--- src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -5,6 +5,7 @@ import mate.academy.spring.dao.AbstractDao;
 import mate.academy.spring.dao.RoleDao;
 import mate.academy.spring.exception.DataProcessingException;
 import mate.academy.spring.model.Role;
+import mate.academy.spring.model.RoleName;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
@@ -21,7 +22,7 @@ public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
         try (Session session = factory.openSession()) {
             Query<Role> findByName = session.createQuery(
                     "FROM Role WHERE name = :name", Role.class);
-            findByName.setParameter("name", name);
+            findByName.setParameter("name", RoleName.valueOf(name));
             return findByName.uniqueResultOptional();
         } catch (Exception e) {
             throw new DataProcessingException("Role with name " + name + " not found", e);
